eaccb331088797375e4087e14d7e820f58d38b9f
antoinefa <antoinefa>
2016-12-05 16:02:49 +0100

    Saved recipe: VERBALISATIONLECTRONIQUE.compute_20161122_pve_securite_routiere_sample_geocoded

--- recipes/compute_20161122_pve_securite_routiere_sample_geocoded.py
+++ recipes/compute_20161122_pve_securite_routiere_sample_geocoded.py
@@ -4,7 +4,7 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 # Recipe inputs
-var f=dataiku.Dataset("20161122_pve_securite_routiere_sample"
+f = Dataset("20161122_pve_securite_routiere_sample")
 events = f.get_dataframe()
 
 
