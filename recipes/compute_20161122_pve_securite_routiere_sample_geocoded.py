20632abbe0e3b780362a8d9d116a21ef1b893afc
antoinefa <antoinefa>
2016-12-05 16:08:43 +0100

    Saved recipe: VERBALISATIONLECTRONIQUE.compute_20161122_pve_securite_routiere_sample_geocoded

--- recipes/compute_20161122_pve_securite_routiere_sample_geocoded.py
+++ recipes/compute_20161122_pve_securite_routiere_sample_geocoded.py
@@ -3,11 +3,20 @@ import dataiku as d
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+step=1000
+
+
+def 
+
 # Recipe inputs
 f = d.Dataset("20161122_pve_securite_routiere_sample")
 events = f.get_dataframe()
 
-
+#splitting
+for k in range(step,len(events)+step,step):
+    file="tmp_{}".format(k)
+    events[k-step:k].to_csv(file,sep=";",quotechar='"',index=false)
+    
 
 # Recipe outputs
 out = d.Dataset("20161122_pve_securite_routiere_sample_geocoded")
