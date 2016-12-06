c1efd80be58f3b0c2e246f0d1dc2249a548c3460
antoinefa <antoinefa>
2016-12-06 11:13:39 +0100

    Saved recipe: VERBALISATIONLECTRONIQUE.compute_20161122_pve_securite_routiere_sample_geocoded

--- recipes/compute_20161122_pve_securite_routiere_sample_geocoded.py
+++ recipes/compute_20161122_pve_securite_routiere_sample_geocoded.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import dataiku as d
-import requests
-import io
+import requests, io, codecs
 from collections import OrderedDict
 import os.path
 import pandas as pd, numpy as np
