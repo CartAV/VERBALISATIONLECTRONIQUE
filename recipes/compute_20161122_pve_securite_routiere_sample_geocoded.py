39ff454554799a6cf2b5ee52878578eca129a4ad
antoinefa <antoinefa>
2016-12-06 11:32:23 +0100

    Saved recipe: VERBALISATIONLECTRONIQUE.compute_20161122_pve_securite_routiere_sample_geocoded

--- recipes/compute_20161122_pve_securite_routiere_sample_geocoded.py
+++ recipes/compute_20161122_pve_securite_routiere_sample_geocoded.py
@@ -38,7 +38,7 @@ liste=[]
 for events_subset in f.iter_dataframes(chunksize=500):
     events_subset.to_csv("tmp.csv",sep=";", quotechar='"',index=False)
     adresse_submit("tmp.csv")
-    liste.append(p.read_csv("tmp.csvgeo",sep=";", na_filter=False,dtype=object,index_col=None))
+    liste.append(pd.read_csv("tmp.csvgeo",sep=";", na_filter=False,dtype=object,index_col=None))
     # Insert here applicative logic on each partial dataframe.
     pass    
 events=pd.concat(liste,ignore_index=True)
