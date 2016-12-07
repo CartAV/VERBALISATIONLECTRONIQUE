31a1fd4a28e630a0627c45d32acdf0a9be99ca01
antoinefa <antoinefa>
2016-12-07 19:28:26 +0100

    Saved recipe: VERBALISATIONLECTRONIQUE.compute_20161122_pve_securite_routiere_geo_dum

--- recipes/compute_20161122_pve_securite_routiere_geo_dum.py
+++ recipes/compute_20161122_pve_securite_routiere_geo_dum.py
@@ -4,6 +4,7 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 # Recipe inputs
+print("chargement in memory")
 ds_in = dataiku.Dataset("20161122_pve_securite_routiere_geocoded")
 df = ds_in.get_dataframe()
 
@@ -12,10 +13,12 @@ threshold=0.02*float(nrows)
 
 liste=('REGROUPEMENT_GENRE','MOIS_INFRACTION','MNT_AF','LIBELLE_TYPE_VOIE','LIBELLE_STATUT_DOSSIER','LIBELLE_CLASSE','SEXE_CONTREVENANT','LIBELLE_FAMILLE','LIBELLE_JOUR_INFRACTION','LIBELLE_NUIT','LIBELLE_PLAGE_HORAIRE','LIB_ENTITE','LIBELLE_CORPS','LIB_TRANCHE_DEPASSEMENT')
 for key in liste:
+    printf("développement de la colonne %s" % key)
     df=pd.concat([df,pd.get_dummies(df[key],prefix=key,prefix_sep=" ")],axis=1)
 
 liste=('LIBELLE_NATURE','MARQUE','NATIONALITE_PLAQUE')
 for key in liste:
+    printf("développement de la colonne %s" % key)
     values = df[key]
     counts = pd.value_counts(values)
     #filtre les valeurs présentées à moins de (threshold) %
