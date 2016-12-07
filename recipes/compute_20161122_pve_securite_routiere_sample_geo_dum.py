5be811f3f5e41b3a1044807ed10fb625cff37c08
antoinefa <antoinefa>
2016-12-07 18:32:41 +0100

    Saved recipe: VERBALISATIONLECTRONIQUE.compute_20161122_pve_securite_routiere_sample_geo_dum

---
+++ recipes/compute_20161122_pve_securite_routiere_sample_geo_dum.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Recipe inputs
+ds_in = dataiku.Dataset("20161122_pve_securite_routiere_sample_geocoded")
+df = ds_in.get_dataframe()
+
+df=pd.concat([df,pd.get_dummies('GENRE')],axis=1)
+
+
+# Recipe outputs
+ds_out = dataiku.Dataset("20161122_pve_securite_routiere_sample_geo_dum")
+ds_out.write_with_schema(df)
